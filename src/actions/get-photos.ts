536c99f28ceaebba9d9e16bc1ee907bf26c135f0
Dhyon Keyllo <dhyon.kpm@hotmail.com>
2024-12-16 04:26:18 -0300

    feat: add project custom api to get photos

--- src/actions/get-photos.ts
+++ src/actions/get-photos.ts
@@ -1,19 +1,19 @@
 "use server";
 
+import { PHOTOS_GET } from "@/functions/api";
 import { Photo } from "@/shared/types";
 
 export async function getPhotos() {
-  const response = await fetch(
-    "https://dogsapi.origamid.dev/json/api/photo/?_page=1&_total=6&_user=0",
-    {
-      next: {
-        // add 10s to revalidate cache
-        revalidate: 10,
-        // add tags ["photo"] to call revalidateTags on post a new foto in post-photo action
-        tags: ["photos"],
-      },
-    }
-  );
+  const { url } = PHOTOS_GET({ page: 1, total: 6, user: 0 });
+
+  const response = await fetch(url, {
+    next: {
+      // add 10s to revalidate cache
+      revalidate: 10,
+      // add tags ["photo"] to call revalidateTags on post a new foto in post-photo action
+      tags: ["photos"],
+    },
+  });
 
   const data = (await response.json()) as Photo[];
 
