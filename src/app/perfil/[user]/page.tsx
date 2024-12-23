46b1ba2cfbb2268ff8daf46e885046ab4f7fc84c
Dhyon Keyllo <dhyon.kpm@hotmail.com>
2024-12-23 01:47:20 -0300

    feat: update PerfilUserPage to fetch and display user photos

--- src/app/perfil/[user]/page.tsx
+++ src/app/perfil/[user]/page.tsx
@@ -1,11 +1,19 @@
-export default function PerfilUserPage({
+import { getPhotos } from "@/actions";
+import { Feed } from "@/components/feed";
+
+export default async function PerfilUserPage({
   params,
 }: {
   params: { user: string };
 }) {
+  const { data } = await getPhotos({ user: params.user });
+
+  if (!data) return <></>;
+
   return (
-    <main>
-      <h1>{params.user}</h1>
-    </main>
+    <section className="container mainSection">
+      <h1 className="title">{params.user}</h1>
+      <Feed photos={data} username={params.user} />
+    </section>
   );
 }
